d39ac8399936540bd504340e238eb1386b420c7c
Yury <work@websitecoder.ru>
2020-10-09 21:48:20 +0300

    number&str

--- js/script.js
+++ js/script.js
@@ -68,82 +68,125 @@
 //     console.log(i);
 // } 
 
-function showFirstMassage() {
-    console.log('hello');
-}
-showFirstMassage();  //обязательно вызвать
-                    //аргументы в ковычках
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// hello
+// function showFirstMassage() {
+//     console.log('hello');
+// }
+// showFirstMassage();  //обязательно вызвать
+//                     //аргументы в ковычках
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // hello
 
-function showFirstMassage(text) {
-    console.log(text);
-    let a = 20;               // переменная остается только внутри функции
-}
-showFirstMassage('hello world!');  
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// hello world!
+// function showFirstMassage(text) {
+//     console.log(text);
+//     let a = 20;               // переменная остается только внутри функции
+// }
+// showFirstMassage('hello world!');  
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // hello world!
 
-let a = 20;               //глобальная переменная
-function showFirstMassage(text) {
-    console.log(text);
-    a = 10;               // глобальную может изменять
-}
-showFirstMassage('hello world!'); 
-console.log(a);
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// hello world!
-// 10
+// let a = 20;               //глобальная переменная
+// function showFirstMassage(text) {
+//     console.log(text);
+//     a = 10;               // глобальную может изменять
+// }
+// showFirstMassage('hello world!'); 
+// console.log(a);
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // hello world!
+// // 10
 
-//внутри ф-ции можно создать переменную с именем глобальной, 
-// она останется в функции и не будет влиять на глобальную
-let an = 20;               
-function showFirstMassage(text) {
-    console.log(text);
-    let an = 10;               
-}
-showFirstMassage('hello world!'); 
-console.log(an);
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// hello world!
-// 20
+// //внутри ф-ции можно создать переменную с именем глобальной, 
+// // она останется в функции и не будет влиять на глобальную
+// let an = 20;               
+// function showFirstMassage(text) {
+//     console.log(text);
+//     let an = 10;               
+// }
+// showFirstMassage('hello world!'); 
+// console.log(an);
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // hello world!
+// // 20
 
-//замыкание функции
-// функция ищет пременную сразу внутри себя, если не находт ищет выше от начала документа
+// //замыкание функции
+// // функция ищет пременную сразу внутри себя, если не находт ищет выше от начала документа
 
-function calc(a, b) {
-    return (a + b);     // возвращает рузультат функции и останавливает функцию
-}
-console.log(calc(2, 5));
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// 7 
+// function calc(a, b) {
+//     return (a + b);     // возвращает рузультат функции и останавливает функцию
+// }
+// console.log(calc(2, 5));
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // 7 
 
-// return может вернуть локальную переменную
-function ret () {
-    let num = 50;
+// // return может вернуть локальную переменную
+// function ret () {
+//     let num = 50;
 
-    //код какой нить
+//     //код какой нить
 
-    return num;
-}
-const anotherNum = ret(); //результат функции в переменную
-console.log(anotherNum);
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// 50
+//     return num;
+// }
+// const anotherNum = ret(); //результат функции в переменную
+// console.log(anotherNum);
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // 50
 
-const logger = function() {
-    console.log('hello');
-};
+// const logger = function() {
+//     console.log('hello');
+// };
 
-logger();
-// [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
-// hello
+// logger();
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // hello
 
-//стрелочные функции
-const calcul = (a, b) =>  a + b ; //если в одну строку
-const calculate = (a, b) => {     // {} если не одна строка
-    console.log('hello');
-    return a + b;
-};
-console.log(calcul(2, 5));
-console.log(calculate(3, 7));  
+// //стрелочные функции
+// const calcul = (a, b) =>  a + b ; //если в одну строку
+// const calculate = (a, b) => {     // {} если не одна строка
+//     console.log('hello');
+//     return a + b;
+// };
+// console.log(calcul(2, 5));
+// console.log(calculate(3, 7)); 
+// // [Running] node "d:\2020\udemy\js\js -2020\js\tempCodeRunnerFile.js"
+// // 7
+// // hello
+// // 10 
+
+//свойства страки
+const str = 'test';
+const arr = [1, 2, 5, 6,7];
+
+console.log(str.length);     //длинна строки
+console.log(arr.length);      //кол-во элементов в массиве
+// [Running] node "d:\2020\udemy\js\js-2020\js\tempCodeRunnerFile.js"
+// 4
+// 5
+//методы строки
+console.log(str.toUpperCase());   // TEST, но переменную не меняет
+console.log(str.toLowerCase());   // в нижний регистр
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit'));   //возвращает индекс первого символа искомого текста
+// [Running] node "d:\2020\udemy\js\js-2020\js\tempCodeRunnerFile.js"
+// 5
+const logg = 'Hello World!';
+console.log(logg.slice(6, 11));    // получить часть строки (индекс начала, индекс сл полсе последнего )
+console.log(logg.substring(6, 11));  // тоже
+console.log(logg.substr(6, 5));      // получить часть строки (индекс начала, количество символов )
+// [Running] node "d:\2020\udemy\js\js-2020\js\tempCodeRunnerFile.js"
+// World
+// World
+// World
+
+//цифры
+const num = 12.2;
+console.log(Math.round(num));   //Math -- библиотека свойств, округление до целого
+// [Running] node "d:\2020\udemy\js\js-2020\js\tempCodeRunnerFile.js"
+// 12
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
+// [Running] node "d:\2020\udemy\js\js-2020\js\tempCodeRunnerFile.js"
+// 12        обе возвращают числовой тип данных, до целого
+// 12.2        дробное
+
+
